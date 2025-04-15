dba13dde46b739b0f92bde934e964a49c88799b3
Devesh <deveshkumar21110@gmail.com>
2025-04-15 15:14:24 +0530

    update new mapping

--- frontend/mylib-frontend/src/config.js
+++ frontend/mylib-frontend/src/config.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = 'https://api.pulib.site/';
+export const API_BASE_URL = 'https://api.pulib.site';
 
 // Other configuration constants can be added here
 export const APP_NAME = 'Readify';
